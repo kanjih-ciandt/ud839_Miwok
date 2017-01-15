2318ba5e8f7797a65ef5468319822e5a11a45e3b
kanjih <kanjih@ciandt.com>
2017-01-15 15:15:07 -0200

    finished lesson 4

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,5 +1,7 @@
 package com.example.android.miwok;
 
+import android.media.Image;
+
 /**
  * Created by kneto on 12/4/16.
  */
@@ -8,10 +10,33 @@ public class Word {
 
     private String defaultWord;
     private String minworkWord;
+    private int imageResourceId;
+    private int soundResourceId;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    private static final int NO_SOUND_PROVIDED = -1;
+
+
+    public Word(String defaultWord, String minworkWord, int imageResourceId, int soundResourceId) {
+        this.defaultWord = defaultWord;
+        this.minworkWord = minworkWord;
+        this.imageResourceId = imageResourceId;
+        this.soundResourceId = soundResourceId;
+    }
+
+    public Word(String defaultWord, String minworkWord, int imageResourceId) {
+        this.defaultWord = defaultWord;
+        this.minworkWord = minworkWord;
+        this.imageResourceId = imageResourceId;
+        this.soundResourceId = NO_SOUND_PROVIDED;
+    }
 
     public Word(String defaultWord, String minworkWord) {
         this.defaultWord = defaultWord;
         this.minworkWord = minworkWord;
+        this.imageResourceId = NO_IMAGE_PROVIDED;
+        this.soundResourceId = NO_SOUND_PROVIDED;
     }
 
     public String getDefaultWord() {
@@ -31,4 +56,19 @@ public class Word {
     }
 
 
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
+    }
+
+    public int getSoundResourceId() {
+        return soundResourceId;
+    }
+
+    public void setSoundResourceId(int soundResourceId) {
+        this.soundResourceId = soundResourceId;
+    }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -4,12 +4,15 @@ package com.example.android.miwok;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.Image;
+import android.media.MediaPlayer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,21 +24,33 @@ import static android.R.attr.resource;
 
 public class WordAdapter extends ArrayAdapter<Word>{
 
+
+
     private static final String LOG_TAG = WordAdapter.class.getSimpleName();
 
+    private int colorResource;
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+
+
+
+    public WordAdapter(Activity context, ArrayList<Word> words,  int colorResource) {
+
         super(context, 0, words);
+        this.colorResource = colorResource;
+
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         // Check if the existing view is being reused, otherwise inflate the view
         View listItemView = convertView;
         if(listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
+
+        View textView = listItemView.findViewById(R.id.text_container);
+        textView.setBackgroundResource(colorResource);
 
         // Get the {@link AndroidFlavor} object located at this position in the list
         Word currentWord = getItem(position);
@@ -52,9 +67,18 @@ public class WordAdapter extends ArrayAdapter<Word>{
         // set this text on the number TextView
         defaultTextView.setText(currentWord.getDefaultWord());
 
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        if(currentWord.getImageResourceId() > 0) {
+            imageView.setImageResource(currentWord.getImageResourceId());
+        } else {
+            imageView.setVisibility(imageView.GONE);
+        }
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
         return listItemView;
     }
+
+
+
 }
